a5ae833bb50de1d2550b90cc1b8027272556237b
Molina0127 <guimoltri@gmail.com>
2024-09-11 16:36:11 -0300

    Create Function List Tasks

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = {
-    value: 'Estudar programação 30 minutos por dia',
-    checked: true,
+    value: 'Tomar 3L de água por dia',
+    checked: false,
 }
 
 let metas = [ meta ]
@@ -11,18 +11,46 @@ const cadastrarMeta = async () => {
     const meta = await input({ message: "Digite a meta:"})
 
     if(meta.length == 0) {
-        console.log("A meta não pode ser vazia")
+        console.log('A meta não pode ser vazia.')
         return
     }
 
     metas.push(
         { value: meta, checked: false }
     )
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
 
 }
 
+
 const start = async () => {
-    while (true) {
+    while(true){
         
         const opcao = await select({
             message: "Menu >",
@@ -41,20 +69,20 @@ const start = async () => {
                 }
             ]
         })
-        
-        switch (opcao) {
+
+        switch(opcao) {
             case "cadastrar":
                 await cadastrarMeta()
                 console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")
+                await listarMetas()
                 break
             case "sair":
-                console.log("Até a próxima")
+                console.log('Até a próxima!')
                 return
         }
     }
 }
 
-start()
+start();
