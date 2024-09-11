12428efdd82fb9e1ff3514114afbc8a679bc5950
Molina0127 <guimoltri@gmail.com>
2024-09-11 10:11:18 -0300

    fundamentals 005

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-// hello world!
-const message = "olá, mundo!";
-console.log(message);
-message = "tchau, mundo!";
-console.log(message);
-console.log(message);
-console.log(message);
+const start = () => {
+    while (true) {
+        let opcao = "cadastrar"
+        switch (opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                return
+        }
+    }
+}
+
+start()
